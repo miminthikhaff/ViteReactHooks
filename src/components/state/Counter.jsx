28cd555941bb09fef7400b8ac95601f2392bdfcb
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-22 23:59:08 +0530

    fix: correct formatting and add placeholders for decrement and reset buttons in Counter component

--- src/components/state/Counter.jsx
+++ src/components/state/Counter.jsx
@@ -1,19 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Counter() {
-    const [count, setCount] = useState(0);
-
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-        <h1>
-            Counter: {count} 
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-
-        </h1>
+      <h1>
+        Counter: {count}
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </h1>
+      <button onClick={}>Decrement</button>
+      <button>Reset</button>
     </div>
-  )
+  );
 }
-
 
 export default Counter;
