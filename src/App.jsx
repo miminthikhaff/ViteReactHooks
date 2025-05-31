17d0ed7bb2961f903166e9ea8b739f5c0dcc94da
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-05-31 23:24:37 +0530

    refactor: comment out Cart component import and usage in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import Counter from "./components/state/Counter";
-import Cart from "./components/state/Cart";
+/* import Counter from "./components/state/Counter";
+import Cart from "./components/state/Cart"; */
+import UseEffectExample from "./components/effect/UseEffectExample";
 
 function App() {
   return (
     <div className="container">
       {/* <Counter /> */}
-      <Cart />
+      {/* <Cart /> */}
+      <UseEffectExample />
     </div>
   );
 }
