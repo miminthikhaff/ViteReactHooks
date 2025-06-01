4c8e271b1252ef271fd697bec0f3222c3194ec44
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-06-01 23:49:10 +0530

    feat: implement counter functionality in UseEffectExample component

--- src/components/effect/UseEffectExample.jsx
+++ src/components/effect/UseEffectExample.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function UseEffectExample() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  function handleIncrement1() {
+    setCount1(prevState => prevState + 1);
+  }
+
+  function handleIncrement2() {
+    setCount2(prevState => prevState + 1);
+  }
+
   return (
-    <div>UseEffectExample</div>
-  )
+    <div>
+      <div>
+        <p>Count 1: {count1}</p>
+        <button onClick={handleIncrement1}>Increment Count 1</button>
+      </div>
+      <div>
+        <p>Count 2: {count2}</p>
+        <button onClick={handleIncrement2}>Increment Count 2</button>
+      </div>
+    </div>
+  );
 }
 
 export default UseEffectExample;
