459e383263c24ac8f7d92404b297766efce44a43
Mohamed Inthikhaff <mhdinthikaff@gmail.com>
2025-06-05 23:53:57 +0530

    fix: add missing useEffect for count in UseEffectExample component

--- src/components/effect/UseEffectExample.jsx
+++ src/components/effect/UseEffectExample.jsx
@@ -13,6 +13,10 @@ function UseEffectExample() {
     console.log("Component Mounted Use Effect 2");
   }, [count2]);
 
+    useEffect(() => {
+    console.log("Component Mounted Use Effect 2");
+  }, [count]);
+
   function handleIncrement1() {
     setCount1((prevState) => prevState + 1);
   }
